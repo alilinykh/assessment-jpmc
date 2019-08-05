ecae3e808fd80c7eed9ddb071f3312e40b68f9b4
alilinykh <aililnykh@dtcc.edu>
2019-08-05 19:26:51 -0400

    actually works

--- jpmc-test/src/main/java/PhoneNumbersIterate.java
+++ jpmc-test/src/main/java/PhoneNumbersIterate.java
@@ -4,24 +4,26 @@ public class PhoneNumbersIterate {
 
     public String numbersToLetter(String input) {
         String[] digits = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        Queue<String> list = new LinkedList<>();
+        LinkedList<String> list = new LinkedList<>();
         list.add("");
 
         for (int i = 0; i < input.length(); i++) {
             int num = input.charAt(i) - 48;
             int size = list.size();
             for(int j = 0; j < size; j++) {
-                String temp = list.();
+                String temp = list.pop();
                 for(int z = 0; z < digits[num].length(); z++) {
                     list.add(temp + digits[num].charAt(z));
                 }
             }
         }
         StringBuilder sb = new StringBuilder();
+
         for (String s : list
              ) {
             sb.append(s + ",");
         }
+
 
         return sb.toString();
     }
